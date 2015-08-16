4361d109cbdd1ae90d846e1d376c92bc6308f497
Lorena M <lorena-m@web.de>
2015-08-16 17:53:46 +0200

    Added CharacterMovement- Script -> so far without any animations

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody2D))]
+public class CharacterMovement : MonoBehaviour {
+
+	public float maxSpeed;
+	public float jumpForce;
+
+	private bool facingRight;
+	private Rigidbody2D rb;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.002f;
+	public LayerMask whatIsGround;
+
+	void Awake(){
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate(){
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+
+		float move = Input.GetAxis ("Horizontal");
+
+		rb.velocity = new Vector2 (move * maxSpeed, rb.velocity.y);
+
+		if (move > 0 && !facingRight) {flip();}
+		else if(move < 0 && facingRight){flip();}
+
+	}
+
+	void Update(){
+		if(grounded && Input.GetButtonDown ("Jump")){
+			rb.AddForce(new Vector2(0, jumpForce));
+		}
+	}
+
+	void flip(){
+		facingRight = !facingRight;
+
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+}
---
+++ Assets/Scripts/ColorController.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ColorState{
+	red, 
+	green, 
+	blue, 
+	noCol
+}
+
+public class ColorController : MonoBehaviour {
+
+	//objects to appear/disappear with the different colors
+	private GameObject[] ap_red;  //appears on red (platforms)
+	private GameObject[] dis_green;  //disappears on green (spiders)
+	private GameObject[] dis_blue;  //disappears on blue (blocks)
+
+	private static ColorState state;
+
+
+	void Awake(){
+		ap_red = GameObject.FindGameObjectsWithTag ("ap_red");
+		dis_green = GameObject.FindGameObjectsWithTag ("dis_green");
+		dis_blue = GameObject.FindGameObjectsWithTag ("dis_blue");
+		state = ColorState.noCol;
+		checkState ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		checkState (other);
+	}
+
+	void checkState(){
+		switch (state) {
+			
+		case ColorState.red:
+			state = ColorState.red;
+			deActivate(ap_red, true);
+			deActivate(dis_green, true);
+			deActivate(dis_blue, true);
+			break;
+		case ColorState.green: 
+			state = ColorState.green;
+			deActivate(ap_red, false);
+			deActivate(dis_green, false);
+			deActivate(dis_blue, true);
+			break;
+		case ColorState.blue:
+			state = ColorState.blue;
+			deActivate(ap_red, false);
+			deActivate(dis_green, true);
+			deActivate(dis_blue, false);
+			break;
+		case ColorState.noCol:
+			state = ColorState.noCol;
+			deActivate(ap_red, false);
+			deActivate(dis_green, true);
+			deActivate(dis_blue, true);
+			
+			break;
+		}
+	}
+	void checkState(Collider other){
+		switch (other.tag) {
+			
+		case "red":
+			state = ColorState.red;
+			deActivate(ap_red, true);
+			deActivate(dis_green, true);
+			deActivate(dis_blue, true);
+			break;
+		case "green": 
+			state = ColorState.green;
+			deActivate(ap_red, false);
+			deActivate(dis_green, false);
+			deActivate(dis_blue, true);
+			break;
+		case "blue":
+			state = ColorState.blue;
+			deActivate(ap_red, false);
+			deActivate(dis_green, true);
+			deActivate(dis_blue, false);
+			break;
+		case "noCol":
+			state = ColorState.noCol;
+			deActivate(ap_red, false);
+			deActivate(dis_green, true);
+			deActivate(dis_blue, true);
+			
+			break;
+		}
+	}
+
+	void deActivate( GameObject[] objects, bool stateToBe){
+		foreach(GameObject ob in objects){
+			ob.SetActive (stateToBe);
+		}
+	}
+
+
+}
