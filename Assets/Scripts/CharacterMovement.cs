934857c1feee45a80a7c71a0c71e27c95a9c78ca
Lorena M <lorena-m@web.de>
2015-08-17 12:10:42 +0200

    added first animations- idle and run

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,23 +6,29 @@ public class CharacterMovement : MonoBehaviour {
 
 	public float maxSpeed;
 	public float jumpForce;
+	private float move;
 
-	private bool facingRight;
+	private bool facingRight = false;
 	private Rigidbody2D rb;
 
-	bool grounded = false;
+	private Animator animator;
+
+	bool grounded = true;
 	public Transform groundCheck;
 	float groundRadius = 0.002f;
 	public LayerMask whatIsGround;
 
 	void Awake(){
 		rb = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
 	}
 
 	void FixedUpdate(){
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
 
-		float move = Input.GetAxis ("Horizontal");
+		move = Input.GetAxis ("Horizontal");
+
+
 
 		rb.velocity = new Vector2 (move * maxSpeed, rb.velocity.y);
 
@@ -32,6 +38,8 @@ public class CharacterMovement : MonoBehaviour {
 	}
 
 	void Update(){
+		animator.SetFloat ("Speed", Mathf.Abs (move));
+
 		if(grounded && Input.GetButtonDown ("Jump")){
 			rb.AddForce(new Vector2(0, jumpForce));
 		}
